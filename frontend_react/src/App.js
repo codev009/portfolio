68d1469b817207714e1b331432b90cc5cd72317e
codev009 <antoney007i@gmail.com>
2022-02-19 22:01:44 +0530

    bare structure set

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 import { About, Footer, Header, Skills, Testimonials, Work} from './container';
+import { Navbar} from './components';
+
 const App = () => {
     return (
             <div className="app">
+                <Navbar/>
             <Header />
             <About />
             <Skills />
             <Work />
-            <Testimonial />
+            <Testimonials />
             <Footer />
             </div>
         );
