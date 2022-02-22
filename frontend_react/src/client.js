31fcb5c81c2555241f9cf35e365d137405bf9721
codev009 <antoney007i@gmail.com>
2022-02-22 11:46:57 +0530

    sanity abouts done

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,9 +2,13 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-   projectId: 'jliewf2t',
+   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
    dataset: 'production',
    apiVersion: '2022-02-01',
     useCdn: true,
-    token: 'skve22Mf2MBltGNmQFSoQWCB58pCmPgYXPddb5o87kD83BGJyFvjDcXKBcDNZ5GguW6YkDLZXdEgH3PDeAJhHJdf4Ntar9ADH9I810KCkDgd3AmWq2lgg52Z4VbF2uICFJppldr38w6DohuVgipzDxrr7KWxe7YY8DOhr9rquepewDYbDvfC',
+    token: process.env.REACT_APP_SANITY_TOKEN,
 });
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
